6c478b8e47036032ab6912759193351db6cef7c2
lioncrown617 <bevischow2013@gmail.com>
2026-05-24 14:26:16 +0800

    Update odd.py

--- odd.py
+++ odd.py
@@ -764,4 +764,5 @@ def download_log():
     return jsonify({"error": "Log 不存在，請先開始監察"}), 404
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, port=5001)
